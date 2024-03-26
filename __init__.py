872848f258fb724c5d800536730e3bc642f7a9cb
N0st0rm <149469935+N0st0rm@users.noreply.github.com>
2024-03-26 11:03:52 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -28,6 +28,17 @@ def meteo():
 def hello_world():
     return render_template('hello.html')
 
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
+
+
+@app.route('/commits')
+def hello_world():
+    return render_template('graph_commits.html')
+
 
 @app.route("/rapport/")
 def mongraphique():
